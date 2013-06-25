dbdd796ad308cebb189a6ebdd954b19f595ce191
Flo Feurstein <flofeurstein@gmail.com>
2013-06-25 08:16:48 +0200

    modified pre_config_script.sh to work with DaliDriver module

--- pre_config_script.sh
+++ pre_config_script.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
+#put this into /etc/rc.local
+
 ###########################
 #       GPIO CONFIG       #
 ###########################
-dali_input_port="138"
-dali_output_port="137"
-
-#activate gpio ports
-echo ${dali_output_port} > /sys/class/gpio/export
-echo ${dali_input_port} > /sys/class/gpio/export
-
-#set direction and value to dialout group and make it read and writeable
-chgrp dialout /sys/class/gpio/gpio${dali_input_port}/direction /sys/class/gpio/gpio${dali_input_port}/value
-chgrp dialout /sys/class/gpio/gpio${dali_output_port}/direction /sys/class/gpio/gpio${dali_output_port}/value
-chmod 660 /sys/class/gpio/gpio${dali_input_port}/direction /sys/class/gpio/gpio${dali_input_port}/value
-chmod 660 /sys/class/gpio/gpio${dali_output_port}/direction /sys/class/gpio/gpio${dali_output_port}/value
+insmod /home/ubuntu/drivers/dali/dali_drv.ko
+chgrp dialout /dev/dali_drv
+chmod 660 /dev/dali_drv
 
 ###########################
 #      SERIAL CONFIG      #
